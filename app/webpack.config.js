3faa9dd1c6c2a834456a71819670452281df5ef2
Khaprinin Maxim <maxim.haprinin@thetaray.com>
2020-01-28 22:42:31 +0200

    test command added env. variables adeed

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -53,6 +53,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
     new CaseSensitivePathsPlugin(),
     new HtmlWebpackPlugin({
       hash: true,
