4c3e00b3b00d226951dc122f896c4d5ab0c304b5
Khaprinin Maxim <maxim.haprinin@thetaray.com>
2020-01-30 18:30:56 +0200

    Code review. WIP

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -13,7 +12,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "dist")
+    contentBase: path.resolve(__dirname, "dist"),
+    historyApiFallback: {
+      index: '/'
+    }
   },
   resolve: {
     extensions: [".tsx", ".jsx", ".ts", ".js"]
@@ -35,29 +37,32 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: "ts-loader",
-        exclude: /node-modules/,
-        options: {
-          useCache: true
-        }
+        exclude: /node-modules/
       },
       {
-        test: /\*.(jsx|js)/,
+        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
         query: {
           cwd: __dirname,
-          presets: ['@babel/preset-env']
         }
       },
       {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "eslint-loader",
+        enforce: "pre",
+      },
+      {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.PORT': JSON.stringify(3000)
     }),
     new webpack.HotModuleReplacementPlugin(),
     new CaseSensitivePathsPlugin(),
