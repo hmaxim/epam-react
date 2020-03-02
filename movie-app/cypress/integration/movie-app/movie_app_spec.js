c985df1658f315c1e276074cacef03ff8c117fc2
maxim.haprinin@gmail.com <maxim.haprinin@gmail.com>
2020-03-02 23:09:54 +0200

    cypress fix.

--- movie-app/cypress/integration/movie-app/movie_app_spec.js
+++ movie-app/cypress/integration/movie-app/movie_app_spec.js
@@ -1,51 +1,51 @@
 const getBtn = label => {
-  return cy.get("button").within(() => cy.contains(label));
+  return cy.get('button').within(() => cy.contains(label));
 };
 
-describe("Movie App", () => {
-  it("Navigate to base url", () => {
-    cy.visit("/");
+describe('Movie App', () => {
+  beforeEach(() => {
+    cy.visit('/');  
   });
 
-  it("Should contain title netflixRoulette", () => {
-    cy.contains("netflixRoulette");
+  it('Should contain title netflixRoulette', () => {
+    cy.contains('netflixRoulette');
   });
 
-  describe("Movies list", () => {
-    it("Should contain list of movies or empty message", () => {
-      const movieList = cy.get("div > .movie-poster");
+  describe('Movies list', () => {
+    it('Should contain list of movies or empty message', () => {
+      const movieList = cy.get('div > .movie-poster');
       if (!movieList) {
-        cy.contains("No films found");
+        cy.contains('No films found');
       } else {
-        cy.contains("Titanic");
+        cy.contains('Titanic');
       }
     });
 
-    it("should scroll to the page botom", () => {
-      cy.get("#movie-scroller").scrollTo("bottom");
+    it('should scroll to the page botom', () => {
+      cy.get('#movie-scroller').scrollTo('bottom');  
     });
   });
 
-  describe("Buttons clicks", () => {
-    it("should fill the input and click on the search button", () => {
-      cy.get("input").type("Lord of the rings");
-      getBtn("SEARCH").click();
+  describe('Buttons clicks', () => {
+    it('should fill the input and click on the search button', () => {
+      cy.get('input').type('Lord of the rings');
+      getBtn('SEARCH').click();
     });
 
-    it("should click on genre button", () => {
-      getBtn("Genre").click();
+    it('should click on genre button', () => {
+      getBtn('Genre').click();
     });
 
-    it("should click on genre button", () => {
-      getBtn("Title").click();
+    it('should click on genre button', () => {
+      getBtn('Title').click();
     });
 
-    it("should click on genre button", () => {
-      getBtn("Rating").click();
+    it('should click on genre button', () => {
+      getBtn('Rating').click();
     });
 
-    it("should click on genre button", () => {
-      getBtn("Release Date").click();
+    it('should click on genre button', () => {
+      getBtn('Release Date').click();
     });
   });
 });
